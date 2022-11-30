6719c7f52c2a2b6f831ef66dcbfd1d1bf62fd3f4
pat-nel87 <patnelson2022@gmail.com>
2022-11-30 18:27:42 -0500

    custom test failure message

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,13 +2,37 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    // At its simplest, a test in Rust is a function that’s annotated with the test attribute
     #[test]
-    fn it_works() {
+    fn exploration() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+    // We might also have non-test functions in the tests module to 
+    // help set up common scenarios or perform common operations
+    // so we always need to indicate which functions are tests using the test attribute.
+    #[test]
+    fn another_test() {
+        panic!("Arbitrary panic test!");
+    }
+
+    #[test]
+    fn say_hello() {
+        println!("Hello");
+    }
+
+    #[test]
+    fn custom_message() {
+        let result = add(2, 2);
+        assert!(
+            result == 5,
+            "Result is not equal to 5, value was {}",
+            result
+        );
     }
 }
